203ce611c4d1f8349dbf03a0dc198343a233812a
FraCuboni <fracuboni@gmail.com>
2024-11-21 16:06:00 +0100

    feat: clean props syntax used

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,9 @@
 import './Card.css';
 
-function Card(props) {
+function Card({ title, description, imgURL }) {
 
     // creo proprietà della card
-    const title = props.title;
-    const description = props.description;
-    const imgURL = props.imgURL;
+
 
     return (
         <div className="card">
@@ -20,5 +18,25 @@ function Card(props) {
 
     )
 }
+// function Card(props) {
+
+//     // creo proprietà della card
+//     const title = props.title;
+//     const description = props.desc;
+//     const imgURL = props.imgURL;
+
+//     return (
+//         <div className="card">
+//             <div className="image-box">
+//                 <img className="image" src={imgURL} alt="" />
+//             </div>
+//             <div className="text-box">
+//                 <h2 className="card-title">{title}</h2>
+//                 <p className="card-description">{description}</p>
+//             </div>
+//         </div>
+
+//     )
+// }
 
 export default Card;
