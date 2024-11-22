f17a500f4502fe010d41c0fd207cc37c089c25db
FraCuboni <fracuboni@gmail.com>
2024-11-22 11:02:49 +0100

    feat: created card conditional rendering BASE

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,13 @@
 import './Card.css';
 
-function Card({ title, description, imgURL }) {
+
+
+// RENDERING CONDIZIONALE
+// CON OPERATORE &&
+// controintuitivo ma molto utilizzato
+function Card({ title, description, imgURL, isVisited }) {
+
+
     return (
         <div className="card">
             <div className="image-box">
@@ -9,11 +16,107 @@ function Card({ title, description, imgURL }) {
             <div className="text-box">
                 <h2 className="card-title">{title}</h2>
                 <p className="card-description">{description}</p>
+                {
+                    // se è visitata manda a schermo 
+                    isVisited && <span>Visitata</span>
+                }
+                {
+                    // se non è visitata manda a schermo 
+                    !isVisited && <span>Non Visitata</span>
+                }
             </div>
         </div>
 
     )
+
 }
+export default Card;
+
+// RENDERING CONDIZIONALE
+// CONDIZIONALE CON TERNARIO IN COSTANTE
+// function Card({ title, description, imgURL, isVisited }) {
+
+//     const visitedLabel = isVisited ? "Visitata" : "Non Visitata";
+
+//     return (
+//         <div className="card">
+//             <div className="image-box">
+//                 <img className="image" src={imgURL} alt="" />
+//             </div>
+//             <div className="text-box">
+//                 <h2 className="card-title">{title}</h2>
+//                 <p className="card-description">{description}</p>
+//                 <span>{visitedLabel}</span>
+//             </div>
+//         </div>
+
+//     )
+
+// }
+
+// RENDERING CONDIZIONALE
+// condizionale tag con operatore ternario
+// function Card({ title, description, imgURL, isVisited }) {
+
+//     return (
+//         <div className="card">
+//             <div className="image-box">
+//                 <img className="image" src={imgURL} alt="" />
+//             </div>
+//             <div className="text-box">
+//                 <h2 className="card-title">{title}</h2>
+//                 <p className="card-description">{description}</p>
+
+//                 {isVisited ? <span>Visitata</span> : <span>Non Visitata</span>}
+//             </div>
+//         </div>
+
+//     )
+
+// }
+
+
+// RENDERING CONDIZIONALE
+// CONDIZIONALE TAG CON IF E ELSE
+// function Card({ title, description, imgURL, isVisited }) {
+
+//     if (isVisited) {
+//         return (
+//             <div className="card">
+//                 <div className="image-box">
+//                     <img className="image" src={imgURL} alt="" />
+//                 </div>
+//                 <div className="text-box">
+//                     <h2 className="card-title">{title}</h2>
+//                     <p className="card-description">{description}</p>
+//                     <span>Visitata</span>
+//                 </div>
+//             </div>
+
+//         )
+
+//     } else {
+//         return (
+//             <div className="card">
+//                 <div className="image-box">
+//                     <img className="image" src={imgURL} alt="" />
+//                 </div>
+//                 <div className="text-box">
+//                     <h2 className="card-title">{title}</h2>
+//                     <p className="card-description">{description}</p>
+//                     <span>Non Visitata</span>
+//                 </div>
+//             </div>
+
+//         )
+
+//     }
+
+// }
+
+
+
+// PROPS CON PASSAGGIO DI UN OGGETTO ASSOCIATO A COSTANTI
 // function Card(props) {
 
 //     // creo proprietà della card
@@ -34,5 +137,3 @@ function Card({ title, description, imgURL }) {
 
 //     )
 // }
-
-export default Card;
